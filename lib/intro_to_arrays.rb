3fe6a206a115e5b8f0a5dd3ce9e1fe59c6772aeb
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-03-26 14:53:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,3 +1,15 @@
 def instantiate_new_array
 empty_array = []
 end
+
+def array_with_two_elements
+  boxers = ["Miguel", "Gio"]
+end
+
+def first_element(array)
+  array[0]
+end 
+
+def last_element(array)
+  array.last
+end
